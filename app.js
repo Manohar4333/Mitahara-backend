0ea9de1d67b012f4563b723425c8bc16653cd3bc
Manohar4333 <thappetlamanohar@gmail.com>
2026-03-13 19:55:49 +0530

    some changes in app file

--- app.js
+++ app.js
@@ -77,6 +77,11 @@ app.use(function (req, res, next) {
 // });
 
 // error handler
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(3000, () => console.log('Server running on port 3000'));
+}
+
+
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
